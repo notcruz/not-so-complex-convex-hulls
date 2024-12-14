ce354c5c2f23dee4d4d586be20b3edc0822b4a34
oofus51 <ltr6274@rit.edu>
2024-12-13 18:23:29 -0600

    Simplified chans code block

--- lib/config.ts
+++ lib/config.ts
@@ -125,24 +125,16 @@ export const algorithms: AlgorithmDetails[] = [
     code:
       `def chans(points):
       n = len(points)
-      for(m = 2; m <= n; m *= m):
+      m = 2
+      while(m <= n):
         mini_hulls = [] # Group up points arbitrarily
         for(i = 0; i < n; i += m):
           current_hull = graham_scan(points[i, i + m])
           mini_hulls.push(current_hull)
-        """
-        Here, jarvis_helper is just like jarvis_march but
-        with two main differences:
-        1) There is a limit of m iterations on the main outer loop,
-        and jarvis_helper will return an empty list if it has an
-        unfinished hull by the end of those m iteration.
-        2) Instead of using all the points of the minihulls, 
-        jarvis_helper will find the upper tangent of each hull for
-        the current_point using binary search.
-        """
         hull_maybe = jarvis_helper(mini_hulls, m)
         if(hull_maybe != []):
-          return hull_maybe`,
+          return hull_maybe
+        m = min(m*m, n)`,
     resources: [],
     steps: {
       "begin": "To begin, Chan's Algorithm will arbitrarily subsdivide the points into groups of at most m, \
